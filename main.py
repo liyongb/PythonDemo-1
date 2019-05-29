4ec48bedc9611b837720ead55d089979ab95f7ae
liyongb <51103985+liyongb@users.noreply.github.com>
2019-05-29 09:37:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print('**************master adding ******************')
 print('**************adding now*******************')
 print('**************adding from master branch')
 print('**************adding from pythonD branch*************')
-print('*************adding fr**************')
+print('*************1**************')
 
 
 
